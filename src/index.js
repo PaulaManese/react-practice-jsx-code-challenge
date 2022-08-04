1ea89bfe91dac1c6f39a974b57fce3302283eec4
Paula Manese <41752060+PaulaManese@users.noreply.github.com>
2022-08-04 15:40:38 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>HELLO! My Name is Paula</h1>
+    <ul>
+      <li>I love travelling</li>
+      <li>I love baking</li>
+      <li>I love coding</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
